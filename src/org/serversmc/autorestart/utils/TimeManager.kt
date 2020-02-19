350197e5b9f81a293fba000642476f3798aafecc
Dennis Lysenko <dennislysenko@hotmail.com>
2020-02-18 21:10:41 -0500

    Updated calculateInterval() algorithm for multiple interval factors "hours and days"

--- src/org/serversmc/autorestart/utils/TimeManager.kt
+++ src/org/serversmc/autorestart/utils/TimeManager.kt
@@ -2,13 +2,21 @@ package org.serversmc.autorestart.utils
 
 import org.serversmc.autorestart.core.*
 import org.serversmc.utils.*
+import org.serversmc.utils.Console.err
 import java.util.*
 import kotlin.collections.ArrayList
 
 object TimeManager {
 	
 	fun calculateInterval() {
-		TimerThread.TIME = (Config.Main_Modes_Interval * 3600.0).toInt()
+		when (Config.Main_Modes_Interval_Factor) {
+			"hours" -> TimerThread.TIME = (Config.Main_Modes_Interval_Value * 3600.0).toInt()
+			"days" -> TimerThread.TIME = (Config.Main_Modes_Interval_Value * 86400.0).toInt()
+			else -> {
+				err("Interval factor \"${Config.Main_Modes_Interval_Factor}\" in 'Main.yml:main.interval.factor' was not found! Switching to 'hours' factor!")
+				TimerThread.TIME = (Config.Main_Modes_Interval_Value * 3600.0).toInt()
+			}
+		}
 	}
 	
 	fun calculateTimestamp() {
@@ -18,8 +26,8 @@ object TimeManager {
 		// Convert timestamps to differences in milliseconds
 		timestamps.forEach {
 			// Check if timestamp is valid
-			if (it.h < 0 || it.h > 23) Console.err("$it hour mark is out of range: 0 - 23")
-			if (it.m < 0 || it.m > 59) Console.err("$it minute mark is out of range: 0 - 59")
+			if (it.h < 0 || it.h > 23) err("$it hour mark is out of range: 0 - 23")
+			if (it.m < 0 || it.m > 59) err("$it minute mark is out of range: 0 - 59")
 			// Add converted time to differences list
 			differences.add(it.getTimeInMillis() - Calendar.getInstance().timeInMillis)
 		}
