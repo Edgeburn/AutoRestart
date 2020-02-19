0882328f076804eadbc0d4b30e65720e50664305
Dennis Lysenko <dennislysenko@hotmail.com>
2020-02-18 19:24:27 -0500

    Moved TimeManager to separate file

--- src/org/serversmc/autorestart/core/TimerThread.kt
+++ src/org/serversmc/autorestart/core/TimerThread.kt
@@ -2,45 +2,11 @@ package org.serversmc.autorestart.core
 
 import org.bukkit.*
 import org.serversmc.autorestart.core.Main.Companion.AutoRestart
-import org.serversmc.autorestart.core.TimeManager.calculateInterval
-import org.serversmc.autorestart.core.TimeManager.calculateTimestamp
 import org.serversmc.autorestart.utils.*
+import org.serversmc.autorestart.utils.TimeManager.calculateInterval
+import org.serversmc.autorestart.utils.TimeManager.calculateTimestamp
 import org.serversmc.utils.Console.consoleSender
 import org.serversmc.utils.Console.err
-import org.serversmc.utils.Console.warn
-import java.util.*
-import kotlin.collections.ArrayList
-
-object TimeManager {
-	
-	fun calculateInterval() {
-		TimerThread.TIME = (Config.Main_Modes_Interval * 3600.0).toInt()
-	}
-	
-	fun calculateTimestamp() {
-		// Initialize variables
-		val timestamps = Config.Main_Modes_Timestamp
-		val differences = ArrayList<Long>()
-		// Convert timestamps to differences in milliseconds
-		timestamps.forEach {
-			// Check if timestamp is valid
-			if (it.h < 0 || it.h > 23) err("$it hour mark is out of range: 0 - 23")
-			if (it.m < 0 || it.m > 59) err("$it minute mark is out of range: 0 - 59")
-			// Add converted time to differences list
-			differences.add(it.getTimeInMillis() - Calendar.getInstance().timeInMillis)
-		}
-		// Check if list is empty
-		if (differences.isEmpty()) {
-			warn("There are no accepted timestamps available! Please check config to ensure that you have followed the correct format.")
-			return
-		}
-		// Get smallest difference
-		val time = differences.min()!!
-		// Convert milliseconds to time
-		TimerThread.TIME = time.toInt() / 1000
-	}
-	
-}
 
 object TimerThread {
 	
